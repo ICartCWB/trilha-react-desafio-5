854db7e77d3ddaaa837f9b3be550504fae6129f0
ICartCWB <israelnuneseng@gmail.com>
2023-01-17 02:10:38 -0300

    Desafio finalizado

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
+export const api = axios.create({
+  baseURL: 'https://prmkcjebnaijfcrnzjmn.supabase.co/rest/v1',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBybWtjamVibmFpamZjcm56am1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM5MDkxMDEsImV4cCI6MTk4OTQ4NTEwMX0.jmumEgw9MQTCodZA0U0-2jAmEgk7wxU2SqLEWmr-FDc',
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBybWtjamVibmFpamZjcm56am1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM5MDkxMDEsImV4cCI6MTk4OTQ4NTEwMX0.jmumEgw9MQTCodZA0U0-2jAmEgk7wxU2SqLEWmr-FDc',
+  },
+});
